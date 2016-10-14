449da351b5f52c98eab9902b49fe5485a4ccd3e8
tibyte <ttl74283@gmail.com>
2016-10-14 23:27:37 +0900

    Create NdkTest2

--- C/pointer/2d.c
+++ C/pointer/2d.c
@@ -3,13 +3,16 @@
 int main()
 {
 	int arr1[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
-	int (*arr2)[3] = arr1;
+	int (*arr2)[3] = (int(*)[3])arr1;
 	
 	for(int i=0; i<3; i++) {
 		for(int j=0; j<3; j++) {
-			printf("%d ", arr2[i][j]);
+			printf("%p\n", arr2[i]);
 		}
 	}
 	
 	return 0;
 }
+
+
+
