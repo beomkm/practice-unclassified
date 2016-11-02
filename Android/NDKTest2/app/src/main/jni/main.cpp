33493d65dcf0b25be4f0e873f0feefc0f88ae2d2
tibyte <tibyte@naver.com>
2016-11-02 23:53:34 +0900

    Modify NDKTest2

---
+++ Android/NDKTest2/app/src/main/jni/main.cpp
@@ -0,0 +1,10 @@
+#include <jni.h>
+#include "kr_tibyte_ndktest2_MainActivity.h"
+
+extern "C" {
+
+JNIEXPORT jstring JNICALL Java_kr_tibyte_ndktest2_MainActivity_getNativeText(JNIEnv *env, jobject obj)
+{
+    return env->NewStringUTF("Native text");
+}
+}
--- Android/NdkTest2/app/src/main/java/kr/tibyte/ndktest2/MainActivity.java
+++ Android/NdkTest2/app/src/main/java/kr/tibyte/ndktest2/MainActivity.java
@@ -1,13 +1,28 @@
 package kr.tibyte.ndktest2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+{
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView)findViewById(R.id.text_view);
+
+        tv.setText(getNativeText());
     }
+
+    static
+    {
+        System.loadLibrary("getNativeText");
+    }
+    public native String getNativeText();
 }
