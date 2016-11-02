62cada61e65ed8db0259545b32e3df950451c845
tibyte <tibyte@naver.com>
2016-11-03 00:03:45 +0900

    Modify NDKTest2

--- Android/NDKTest2/app/src/main/jni/main.cpp
+++ Android/NDKTest2/app/src/main/jni/main.cpp
@@ -2,7 +2,6 @@
 #include "kr_tibyte_ndktest2_MainActivity.h"
 
 extern "C" {
-
 JNIEXPORT jstring JNICALL Java_kr_tibyte_ndktest2_MainActivity_getNativeText(JNIEnv *env, jobject obj)
 {
     return env->NewStringUTF("Native text");
